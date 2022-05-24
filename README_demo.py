c7dda1f27c29ae019c5730ac8e13cf5d53ea1b0c
RKatzChemComp <101732585+RKatzChemComp@users.noreply.github.com>
2022-05-24 17:30:45 -0400

    Update README_demo.py

--- README_demo.py
+++ README_demo.py
@@ -3,17 +3,19 @@ READ ME
 Set up to run the demo file:
     1. Import all support pakages: matplotlib, skimage, cv2, numpy, imutils
     2. Insure demo image file is in the same directory as the watershead code (uwatershead_ind_2color.py)
+    Install Time should be less than 1 minute to download the code. Code can be run on both a lixnus and windows OS 
 Run Segmentation 
     1. Run the watershead segmentation code (uwatershead_ind_2color.py) 
     2. Move segmented particles (all the generated png image file of individual rods) into a directory with the cntrl_file and lables code
         - Note: Depending on the image the scale bar or agrigated particles may have been captured. Do not take these into the new directory.
-        Clustered particles will generate false catigories and images without regions (eg. only white from the border) defined in cntrl_file will cause the code to fail 
+        Clustered particles will generate false catigories and images without regions (eg. only white from the border) defined in cntrl_file will cause the code to fail
+    - Run time should be less than 2 minutes
 Run Lables
     1. Insure that the HSV color codes in the cntrl_file match the regions of intrest in the particle
     2. Run the lables code (Lables_v2.5.py), calling the cntrl_file from the command line 
     3. The code will generate lists of particles in the same catigory and the number in each catigory as well as an array iforming you of the regions and the order they were found in
        For example:"demo.jpg.14 as array (25.,102), array (94, 102)" means the particle represented in demo.jpg.14 is a rod with a blue region and a green region, which is a CdS-ZnS rod
-
+     - Run time should be less than 2 minutes
 Optimization for Files beyond the demo file:
     1. Optimzie the thresholding, itterations of opening and closing to optain cleanly segemented particles
             Look at the documentation for opencv as needed
